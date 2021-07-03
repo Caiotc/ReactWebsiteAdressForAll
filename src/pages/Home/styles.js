3c0d28f8bfe443baf5835b7a5ec47277dad0adba
Caio theodoro <caio.theodoro@hotmail.com>
2021-07-03 10:26:44 -0300

    structure definition

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const HomeContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: red;
+    height: 100vh;
+`;
+export const Title = styled.h1`
+    color:purple;
+`;
